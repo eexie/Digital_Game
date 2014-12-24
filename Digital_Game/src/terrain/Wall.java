80ee406f856cc3be04e40ca752a783c80e0bc887
eexie <emmawfx4@gmail.com>
2014-12-24 18:25:27 -0500

    Update Wall.java

--- Digital_Game/src/terrain/Wall.java
+++ Digital_Game/src/terrain/Wall.java
@@ -2,16 +2,26 @@ package terrain;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Wall extends Terrain {
-	public Wall(int x,int y){
-		super(x,y);
-		super.solid=true;
+	Rectangle collisionBox = new Rectangle(0, 0, 0, 0);
+
+	public Wall(int x, int y) {
+		super(x, y);
+		super.solid = true;
+		collisionBox.setBounds(x, y, 50, 50);
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.red);
-		g.fillOval(pos.getX(),pos.getY(),50,50);
+		g.fillOval(x, y, 50, 50);
+
+		g.drawRect(x, y, 50, 50);
+	}
+
+	public Rectangle getRect() {
+		return collisionBox;
 	}
 }
