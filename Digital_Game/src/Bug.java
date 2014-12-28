071ce17652d1a4e24b57ee808aad4becfecf29a4
eexie <emmawfx4@gmail.com>
2014-12-27 23:34:02 -0500

    Update Bug.java

--- Digital_Game/src/Bug.java
+++ Digital_Game/src/Bug.java
@@ -4,6 +4,7 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 
+import terrain.Terrain;
 import terrain.Wall;
 
 public class Bug extends Unit {
@@ -21,31 +22,30 @@ public class Bug extends Unit {
 		damage = 0;
 		health = 80 + 20 * type;
 	}
-
+	
 	public int getType() {
 		return type;
 	}
-
+	
 	public void setType(int type) { // types are added together for new type
 		this.type = type;
 	}
-
+	
 	public void updateImage() {
 		timg = new ImageIcon("t" + type + ".png").getImage();
 	}
-
+	
 	public void draw(Graphics g) {
 		g.setColor(Color.RED);
 		g.drawImage(timg, x - size, y - size, size * 2, size * 2, null);
 		if (selected)
 			g.drawRect(x - size, y - size, size * 2, size * 2);
-
 	}
-
+	
 	public Image getImage() {
 		return timg;
 	}
-
+	
 	public void attack() {
 		switch (type) {
 		case 1:
@@ -69,30 +69,38 @@ public class Bug extends Unit {
 	public int getY() {
 		return y;
 	}
-
+	
 	public void fire(int distance) {
 		Bullet newBullet = new Bullet(x, y, distance);
 		// move toward nearest enemy
 		Game.bullets.add(newBullet);
 	}
-
+	
 	public int getHealth() {
 		return health;
 	}
-
-	public void update() {
-		super.update();
-		System.out.println("run");
-		rect.setRect(x - size, x - size, size, size); // collision rect
-		for (Wall i : World.obstacles) {
-			if (rect.intersects(i.getRect())) {
-				System.out.println("yes");
+	
+	public void update(World map) { // collision rect
+		
+		for (Terrain i : map.getSect().getMap()){
+			if (i.getSolid()&&super.getRect().intersects(i.getRect())) {
+				if(this.x>=i.getX())
+					x=i.getX()+i.getSize()+1;
+				else if(this.x<i.getX())
+					x=i.getX()-i.getSize()-1;
+				if(this.y>=i.getY())
+					this.y=i.getY()+i.getSize()+1;
+				else if(this.y<i.getY())
+					y=i.getY()-i.getSize()-1;
 				tx=x;
 				ty=y;
 			}
+			else{
+				super.update();
+			}
 		}
 	}
-
+	
 	public void moveTo(int tx, int ty) {
 		this.tx = tx;
 		this.ty = ty;
@@ -101,15 +109,15 @@ public class Bug extends Unit {
 		this.ty += (int) (Math.random() * 50);
 		this.ty -= (int) (Math.random() * 50);
 	}
-
+	
 	public void setHealth(int health) {
 		this.health = health;
 	}
-
+	
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
-
+	
 	public int getDamage() {
 		return damage;
 	}
