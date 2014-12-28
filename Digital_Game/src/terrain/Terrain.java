77c8bfeda9b25343c8a92caf5be8ad262f645abc
eexie <emmawfx4@gmail.com>
2014-12-27 23:34:40 -0500

    Update Terrain.java

--- Digital_Game/src/terrain/Terrain.java
+++ Digital_Game/src/terrain/Terrain.java
@@ -1,16 +1,45 @@
 package terrain;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public abstract class Terrain {
-	protected Vector pos;
+	protected int x;
+	private int y;
 	protected boolean solid;
 	protected String type;
+	private int size=25;
 	public Terrain(int x,int y){
-		pos=new Vector(x,y);
+		this.setX(x);
+		this.setY(y);
 	}
 	public boolean getSolid(){
 		return solid;
 	}
 	public abstract void draw(Graphics g);
+	
+	public Rectangle getRect(){
+		return new Rectangle(getX(),getY(),getSize()*2,getSize()*2);
+	}
+	public String toString(){
+		return "Position: "+getX()+" "+getY();
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 }
