d5fe4dd9a8747c49424fed771403a3753d12ea7d
eexie <emmawfx4@gmail.com>
2015-01-05 10:11:05 -0500

    Update DefPanel.java

--- Digital_Game/src/DefPanel.java
+++ Digital_Game/src/DefPanel.java
@@ -12,7 +12,7 @@ public class DefPanel extends JPanel {
 	public JButton toMenu;
 	private GridBagConstraints gbc;
 
-	public DefPanel(String title,String text) {
+	public DefPanel(String title) {
 		// TODO Auto-generated constructor stub
 		setBackground(Color.BLACK);
 		gbc = new GridBagConstraints();
@@ -22,12 +22,9 @@ public class DefPanel extends JPanel {
 		label = new JLabel(title);
 		label.setFont(DefFont.derived(50, 0.4));
 		label.setForeground(Color.WHITE);
-		gbc.insets=new Insets(0,0,100,0);
+		gbc.insets=new Insets(0,0,50,0);
 		
 		add(label, gbc);
-		if(text!=""){
-			addLabel(text);
-		}
 		gbc.insets = new Insets(10, 0, 0, 0);
 		gbc.gridy=20;
 		toMenu = new PrettyBtn("BACK TO MENU",2);
