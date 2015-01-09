cb87c162c23a2ddd8a1d4b099c3a95374bde8bce
eexie <emmawfx4@gmail.com>
2015-01-09 10:17:46 -0500

    Update Bug.java

--- Digital_Game/src/Bug.java
+++ Digital_Game/src/Bug.java
@@ -1,41 +1,78 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 
-public class Bullet extends Unit {
-	private static int size = 5; // width and height
-	private int time, damage;
-	public String type;
+import javax.swing.ImageIcon;
 
-	// constructor
-	public Bullet(int x, int y, int time, int damage, String type) {
+import terrain.Terrain;
+
+public class Bug extends Unit {
+	private int type;
+	private Image timg;
+	static int SEPARATE_THRESHHOLD = 20;
+	public boolean selected;
+	public Bullet bullet;
+	protected int health;
+	public static int size = 25;
+
+	public Bug(int x, int y) {
 		super(x, y, size);
-		this.damage = damage;
-		this.time = time;
+		type = 1;
+		updateImage();
+		health = 80 + 20 * type;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) { // types are added together for new type
 		this.type = type;
 	}
 
-	public void moveTo(int tx, int ty) {
-		if (reached) {
-			this.tx = tx;
-			this.ty = ty;
-		}
+	public void updateImage() {
+		timg = new ImageIcon("t" + type + ".png").getImage();
 	}
 
-	public void update() {
-		time--;
-		if (x >= 770 || y >= 675 || x <= 5 || y <= 5 || time == 0) {
-			if (type.equals("bug")) {
-				Game.bullets.remove(this);
-			} else {
-				Game.enemyBullets.remove(this);
-			}
-		} else {
-			super.update();
-			checkCollision();
+	public void draw(Graphics g) {
+		g.setColor(Color.RED);
+		g.drawImage(timg, x - size, y - size, size * 2, size * 2, null);
+		if (selected) {
+			g.setColor(new Color(240, 233, 34));
+			((Graphics2D) g).setStroke(new BasicStroke(3));
+			g.drawRect(x - size, y - size, size * 2, size * 2);
 		}
+		((Graphics2D) g).setStroke(new BasicStroke(1));
+		g.setColor(Color.GREEN);
+		g.fillRect(x - size, y - size, health / 2, 5);
 	}
 
-	// returning all the necessary value of this class
+	public Image getImage() {
+		return timg;
+	}
+
+	public void attack() {
+		switch (type) {
+		case 1:
+			fire(40, 5);
+			break;
+		case 2:
+			fire(40, 10);
+			break;
+		case 3:
+			fire(80, 5);
+			break;
+		case 4:
+			fire(80, 10);
+			break;
+		case 5:
+			fire(80, 20);
+			break;
+
+		}
+	}
 
 	public int getX() {
 		return x;
@@ -45,64 +82,84 @@ public class Bullet extends Unit {
 		return y;
 	}
 
-	public int getDamage() {
-		return damage;
+	public void fire(int time, int damage) {
+		Bullet newBullet = new Bullet(x, y, time, damage, "bug");
+		// move toward nearest enemy
+		Game.bullets.add(newBullet);
 	}
 
-	public void draw(Graphics g) {
-		switch (damage) {
-		case 2:
-			g.setColor(Color.BLUE);
-			break;
-		case 4:
-			g.setColor(Color.GREEN);
-			break;
-		case 6:
-			g.setColor(Color.RED);
-			break;
-		}
-		g.drawRect(getCollision().x, getCollision().y, getCollision().width,
-				getCollision().height);
-		g.fillRect(super.x - 5, super.y - 5, size * 2, size * 2);
+	public int getHealth() {
+		return health;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
+	public void update(World map) { // collision rect
 
-	public void setY(int y) {
-		this.y = y;
-	}
+		for (Terrain i : map.getSect().getMap()) {
+			if (i.getSolid()
+					&& super.getCollision().intersects(i.getCollision())) {
+				if (Math.abs(tx - x) > Math.abs(ty - y)) {
+					if (x > i.getX())
+						x = i.getX() + i.getSize() + 1 + size;
+					else if (x < i.getX())
+						x = i.getX() - i.getSize() - 1 - size;
+				} else {
+					if (y > i.getY())
+						y = i.getY() + i.getSize() + 1 + size;
+					else if (y < i.getY())
+						y = i.getY() - i.getSize() - 1 - size;
+				}
+				tx = x;
+				ty = y;
+				System.out.println("box" + x + " " + y);
+				System.out.println(i.getX() + " " + i.getY());
+			} else {
+				if (!i.getSolid()
+						&& super.getCollision().intersects(i.getCollision())) {
+					int active = i.active();
+					switch (active) {
+					case 0:
+						break;
+					case 1:
+						System.out.print("We did it, O:");
+						break;
+					case 3:
+						map.change(1);
+						break;
+					case 4:
+						map.change(-1);
+						break;
 
-	private void checkCollision() {
-		if (type.equals("bug")) {
-			for (int i = 0; i < Game.enemies.size(); i++) {
-				Enemy curr = (Enemy) Game.enemies.get(i);
-				if (curr.getBound().intersects(getCollision())) {
-					if (((Enemy) curr).health > 0) {
-						((Enemy) curr).health -= 1;
+					// Check point captured. Show's the captured point.
+
 					}
-					if (((Enemy) curr).health <= 0) {
-						Game.score += 5;
-						Game.enemies.remove(i);
-					}
-					Game.bullets.remove(this);
 				}
-			}
-		} else {
-			for (int i = 0; i < Game.bugs.size(); i++) {
-				Unit curr = Game.bugs.get(i);
-				if (getCollision().intersects(curr.getCollision())) {
-					System.out.println("intersect");
-					((Bug) curr).health -= 1;
-				}
-				if (((Bug) curr).health <= 0) {
-					Game.score += 5;
-					Game.bugs.remove(i);
-				}
-				Game.bullets.remove(this);
+				super.update();
 			}
 		}
 	}
 
+	public void moveTo(int tx, int ty) {
+		this.tx = tx;
+		this.ty = ty;
+		this.tx += (int) (Math.random() * 50);
+		this.tx -= (int) (Math.random() * 50);
+		this.ty += (int) (Math.random() * 50);
+		this.ty -= (int) (Math.random() * 50);
+	}
+
+	public void separate() {
+		for (Unit i : Game.bugs) {
+			if (i == this)
+				continue;
+			int distance = (int) Math.sqrt((Math.pow(this.x - i.x, 2) + Math
+					.pow(this.y - i.y, 2)));
+			if (distance < SEPARATE_THRESHHOLD) {
+
+			}
+		}
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
 }
