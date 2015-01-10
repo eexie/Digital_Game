18ec01700923586f1a417bf5e02bb683cba3f3ca
eexie <emmawfx4@gmail.com>
2015-01-10 18:50:21 -0500

    Update Enemy.java

--- Digital_Game/src/Enemy.java
+++ Digital_Game/src/Enemy.java
@@ -1,20 +1,27 @@
+
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
 
 public class Enemy extends Unit {
 	private static int size;
 	protected int health;
 	private int type;
 	private int cooldown = 50;
+	private int direction;
 
 	public Enemy(int x, int y, int type) {
 		super(x, y, size);
 		this.type = type;
 		size = 20 * type;
 		health = (int) (90 + Math.pow(2, type));
+		if (type == 1||type==2) {
+			direction = (int) (Math.random() * 4) + 1;
+		}
+		System.out.println(direction);
 	}
 
 	public void attack() {
@@ -39,7 +46,7 @@ public class Enemy extends Unit {
 
 	public void update() {
 		cooldown--;
-		if (cooldown == 0) {
+		if (cooldown == 25) {
 			attack();
 			cooldown = 50;
 		}
@@ -51,8 +58,8 @@ public class Enemy extends Unit {
 		drawHCircle(x, y, size, type, g);
 		drawVCircle(x, y, size, type, g);
 		g.setColor(Color.RED);
-		g.fillRect(x - size, (int) (y + size * 3),
-				(int) (health * type / 1.3), 10);
+		g.fillRect(x - size, (int) (y + size * 3), (int) (health * type / 1.3),
+				10);
 	}
 
 	public void drawHCircle(int cx, int cy, int radius, int depth, Graphics g) {
@@ -77,23 +84,19 @@ public class Enemy extends Unit {
 
 	public void fireStraight(int damage) {
 		int x2 = x + size, y2 = y + size;
-		Bullet newBullet = new Bullet(x2, y2, 50, 1, "enemy");
-		int rnd = (int) Math.random() * 4 + 1;
-		switch (rnd) {
+		Bullet newBullet = new Bullet(x2, y2, 30*type, 1, "enemy");
+		switch (direction) {
 		case 1: // move straight left
-			newBullet.tx = 0;
-			newBullet.ty = y2;
+			newBullet.moveTo(0, y2 + 1);
 			break;
 		case 2: // move straight right
-			newBullet.tx = 700;
-			newBullet.ty = y2;
+			newBullet.moveTo(1000, y2);
 			break;
 		case 3: // move straight up
-			newBullet.tx = x2;
-			newBullet.ty = 0;
+			newBullet.moveTo(x2 + 1, 0);
 			break;
 		case 4: // move straight down
-			newBullet.tx = x2;
+			newBullet.moveTo(x2, 700);
 			newBullet.ty = 700;
 			break;
 		}
@@ -101,24 +104,21 @@ public class Enemy extends Unit {
 	}
 
 	public void fireFour(int damage) {
-		for (int i = 0; i < 4; i++) {
+		for (int i = 1; i <= 4; i++) {
 			int x2 = x + size, y2 = y + size;
-			Bullet newBullet = new Bullet(x2, y2, 50, 1, "enemy");
+			Bullet newBullet = new Bullet(x2, y2, 60, 1, "enemy");
 			switch (i) {
 			case 1: // move straight left
-				newBullet.tx = 0;
-				newBullet.ty = y2;
+				newBullet.moveTo(0, y2 + 1);
 				break;
 			case 2: // move straight right
-				newBullet.tx = 700;
-				newBullet.ty = y2;
+				newBullet.moveTo(1000, y2);
 				break;
 			case 3: // move straight up
-				newBullet.tx = x2;
-				newBullet.ty = 0;
+				newBullet.moveTo(x2 + 1, 0);
 				break;
 			case 4: // move straight down
-				newBullet.tx = x2;
+				newBullet.moveTo(x2, 700);
 				newBullet.ty = 700;
 				break;
 			}
@@ -127,49 +127,42 @@ public class Enemy extends Unit {
 	}
 
 	public void fireEight(int damage) {
-		for (int i = 0; i < 6; i++) {
+		for (int i = 1; i <= 8; i++) {
 			int x2 = x + size, y2 = y + size;
-			Bullet newBullet = new Bullet(x2, y2, 50, 1, "enemy");
+			Bullet newBullet = new Bullet(x2, y2, 100, 1, "enemy");
+			int n = (int) (Math.sqrt(1000000));
 			switch (i) {
 			case 1: // move straight left
-				newBullet.tx = 0;
-				newBullet.ty = y2;
+				newBullet.moveTo(0, y2 + 1);
 				break;
 			case 2: // move straight right
-				newBullet.tx = 700;
-				newBullet.ty = y2;
+				newBullet.moveTo(1000, y2);
 				break;
 			case 3: // move straight up
-				newBullet.tx = x2;
-				newBullet.ty = 0;
+				newBullet.moveTo(x2 + 1, 0);
 				break;
 			case 4: // move straight down
-				newBullet.tx = x2;
-				newBullet.ty = 700;
+				newBullet.moveTo(x2, 700);
 				break;
 			case 5: // move diagonal right down
-				newBullet.tx += (int) (Math.sqrt(100));
-				newBullet.ty += (int) (Math.sqrt(100));
+				newBullet.moveTo(newBullet.tx + n + 1, newBullet.ty + n + 1);
 				break;
 			case 6: // move diagonal left up
-				newBullet.tx -= (int) (Math.sqrt(100));
-				newBullet.ty -= (int) (Math.sqrt(100));
+				newBullet.moveTo(newBullet.tx - n - 1, newBullet.ty - n - 1);
 				break;
 			case 7: // move diagonal right up
-				newBullet.tx += (int) (Math.sqrt(100));
-				newBullet.ty -= (int) (Math.sqrt(100));
+				newBullet.moveTo(newBullet.tx + n + 1, newBullet.ty - n - 1);
 				break;
 			case 8: // move diagonal left down
-				newBullet.tx -= (int) (Math.sqrt(100));
-				newBullet.ty += (int) (Math.sqrt(100));
+				newBullet.moveTo(newBullet.tx - n - 1, newBullet.ty + n + 1);
 				break;
-
 			}
 			Game.enemyBullets.add(newBullet);
 		}
 	}
 
-	public Rectangle getRect() {
-		return new Rectangle(x, y, size * 2, size * 2);
+	public Ellipse2D.Double getBound() {
+//		return new Rectangle(x, y, size * 2, size * 2);
+		return new Ellipse2D.Double(x, y, size*2, size*2);
 	}
 }
