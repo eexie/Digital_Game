73be0a5199acc1de62d0b7ad74446ffccca30650
eexie <emmawfx4@gmail.com>
2015-01-13 22:38:53 -0500

    Update Wall.java

--- Digital_Game/src/terrain/Wall.java
+++ Digital_Game/src/terrain/Wall.java
@@ -1,21 +1,28 @@
 package terrain;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 
 public class Wall extends Terrain {
+	private Image img = new ImageIcon("wall.png").getImage();
 
 	public Wall(int x, int y) {
 		super(x, y);
 		super.solid = true;
+		super.size = 25;
 	}
 
 	@Override
-	public void draw(Graphics g) {
-		g.setColor(Color.red);
-		g.fillOval(super.x, super.getY(), getSize()*2, getSize()*2);
+	public void draw(Graphics g, int x, int y) {
+		g.drawImage(img, this.x - size - x, this.y - size - y, size * 2 + 3,
+				size * 2 + 3, null);
+	}
 
-		g.drawRect(super.x, super.getY(), getSize()*2, getSize()*2);
+	@Override
+	public int active() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
